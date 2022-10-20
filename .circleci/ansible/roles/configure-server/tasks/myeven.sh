a6f92c46370240c3b0ae72233fd6a45298715221
Aya Rabih <ayarabih21@gmail.com>
2022-10-20 16:17:56 +0200

    configure infrastructure

---
+++ .circleci/ansible/roles/configure-server/tasks/myeven.sh
@@ -0,0 +1,11 @@
+export NODE-ENV=production
+export VERSION=1
+export TYPEORM_CONNECTION=postgres
+export TYPEORM_MIGRATIONS_DIR=./migration
+export TYPEORM_ENTITIES=./modules/domain/**/*.entity{.ts,.js}
+export TYPEORM_MIGRATIONS=./migrations/*{.ts,.js}
+export TYPEORM_HOST=database-1.ce3a1jcrszxw.us-east-1.rds.amazonaws.com
+export TYPEORM_PORT=5532
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=postgres
+export TYPEORM_DATABASE=postgres
